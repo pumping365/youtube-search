fc0b64a497bdc835f25d944dbb2a1ee09e46bb98
pumping365 <163749543+pumping365@users.noreply.github.com>
2026-02-23 13:24:17 +0900

    Add PWA icons and manifest for VIDORA PRO

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+// VIDORA PRO - Service Worker for PWA
+const CACHE_NAME = 'vidora-pro-v1';
+const urlsToCache = [
+  '/',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Clean old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
